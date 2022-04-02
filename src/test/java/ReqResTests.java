5b1106f37513f1af5b4133d0dc700bb97f5a998d
SashkaDikaya <dikayaav@gmail.com>
2022-04-02 17:05:01 +0300

    commit

--- src/test/java/ReqResTests.java
+++ src/test/java/ReqResTests.java
@@ -13,7 +13,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class ReqResTests extends TestBase {
 
     @Test
-    @DisplayName("Вывести информацию по пользователю id 7")
+    @DisplayName("Поиск информации по id пользователя")
     void oneUserTest() {
 
         LombokUserData response = given()
