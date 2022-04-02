59cc89a234e121728c920872cb3e97bc659446fc
SashkaDikaya <dikayaav@gmail.com>
2022-04-02 16:11:05 +0300

    commit

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -16,13 +16,11 @@ public class TestBase {
         String version = System.getProperty("version", "91");
         String size = System.getProperty("size", "1920x1080");
 
-        Configuration.baseUrl = "https://demoqa.com";
+        //Configuration.baseUrl = "https://demoqa.com";
 
         Configuration.browser = browser;
         Configuration.browserVersion = version;
         Configuration.browserSize = size;
-
-       //SelenideLogger.addListener("allure", new AllureSelenide());
 
     }
 
