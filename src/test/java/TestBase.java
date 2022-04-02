535f27587b48c3e8d47387e93fec69804caff2a1
SashkaDikaya <dikayaav@gmail.com>
2022-04-02 16:11:59 +0300

    commit

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -16,8 +16,6 @@ public class TestBase {
         String version = System.getProperty("version", "91");
         String size = System.getProperty("size", "1920x1080");
 
-        //Configuration.baseUrl = "https://demoqa.com";
-
         Configuration.browser = browser;
         Configuration.browserVersion = version;
         Configuration.browserSize = size;
